031ad8ccd99db1bc26e026089abbca9de415e091
Jonathancsr <jonathancsrr@gmail.com>
2019-08-25 20:38:24 -0300

    feat: auth with jwt

---
+++ modulo02/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd3ec088367b363568c582c60965593c9',
+  expiresIn: '7d',
+};
--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionConntroller';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.post('/sesions', SessionController.store);
 
 export default routes;
