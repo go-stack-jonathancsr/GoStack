27d177c0a71ed17017d470a8104c4c28eb40f54f
Jonathancsr <jonathancsrr@gmail.com>
2019-08-25 20:04:38 -0300

    feat: Register user

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,7 +1,8 @@
-const { Router } = require('express');
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'teste' }));
+routes.post('/users', UserController.store);
 
 export default routes;
