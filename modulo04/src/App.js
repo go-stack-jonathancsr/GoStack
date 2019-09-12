8bcdc879c7d3c3179437349688b966c96bc2ce2e
jonathancsr <jonathancsrr@gmail.com>
2019-09-12 14:38:03 -0300

    Modulo 04 aula 6

---
+++ modulo04/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./App.css";
+import TechList from "./components/TechLists";
+import Screen from "./assets/screen.png";
+
+export default function App() {
+  return (
+    <h1>
+      <TechList />
+    </h1>
+  );
+}
